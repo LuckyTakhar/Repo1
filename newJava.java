ed59b9ef9719138c4d88d9b7d6db0081d5f5c699
LuckyTakhar <24001002025lucky@dcrustm.org>
2026-07-12 18:46:47 +0530

    add newJava.java

---
+++ newJava.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class newJava {
+ public static void main(String[] args) {
+    System.out.println("this is first java program for the github");
+    Scanner og = new Scanner(System.in);
+    String name = og.nextLine();
+    System.out.println(name);
+    
+
+ }
+    
+ }
+ 
