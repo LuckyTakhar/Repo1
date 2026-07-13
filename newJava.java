26c2dd471be333c7fd082e11e8178107b5fd19c2
Lucky takhar <24001002025lucky@dcrustm.org>
2026-07-13 21:34:06 +0530

    Add new print statement to newJava program

--- newJava.java
+++ newJava.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class newJava {
  public static void main(String[] args) {
     System.out.println("this is first java program for the github");
+    System.out.println("this is new change from github")
     Scanner og = new Scanner(System.in);
     String name = og.nextLine();
     System.out.println(name);
